a470d6d10f348a3197495b04b964d3101d1493e9
Farkas <farasz@kkszki.hu>
2024-09-17 11:30:27 +0200

    The Employer class is ready to use.

---
+++ Employers/Employers/Employer.cs
@@ -0,0 +1,10 @@
+﻿namespace Employers
+{
+    public class Employer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Sallery { get; set; }
+    }
+}

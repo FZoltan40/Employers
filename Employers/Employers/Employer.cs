be2901bce24f23f1d2ef59e3e3590e6ef9e2a644
Farkas <farasz@kkszki.hu>
2024-09-17 12:19:16 +0200

    gitignore file is added.

--- Employers/Employers/Employer.cs
+++ Employers/Employers/Employer.cs
@@ -2,7 +2,7 @@
 {
     public class Employer
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public int Sallery { get; set; }

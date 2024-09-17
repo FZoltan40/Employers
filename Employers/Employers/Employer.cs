1028cd4ae2714823f7f248fdcd890faf84bc426d
Farkas <farasz@kkszki.hu>
2024-09-17 13:12:22 +0200

    Emplyer constructor ready to use. Input file is added to project.

--- Employers/Employers/Employer.cs
+++ Employers/Employers/Employer.cs
@@ -1,4 +1,6 @@
-﻿namespace Employers
+﻿using System.IO;
+
+namespace Employers
 {
     public class Employer
     {
@@ -6,5 +8,17 @@
         public string Name { get; set; }
         public int Age { get; set; }
         public int Sallery { get; set; }
+
+        public Employer(string file)
+        {
+            StreamReader sr = new StreamReader(file);
+            string[] darabol = sr.ReadLine().Split(';');
+
+            Id = int.Parse(darabol[0]);
+            Name = darabol[1];
+            Age = int.Parse(darabol[2]);
+            Sallery = int.Parse(darabol[3]);
+
+        }
     }
 }
